24e5b0895c645559324792336a77580856800127
OliveriQ <76691188+OliveriQ@users.noreply.github.com>
2021-11-30 14:55:59 +0000

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #include "solver.h"
 
-Solver::Solver(std::vector<int> &board) 
-{
+Solver::Solver(std::vector<int> &board) {
 	this->board = board;
 }
 
-bool Solver::isEmpty(int index)
-{
+bool Solver::isEmpty(int index) {
 	return (board[index] == 0) ? true : false;
 }
 
-bool Solver::isValid(int index)
-{
+bool Solver::isValid(int index) {
 	int row, col;
 	row = index / 9;
 	col = index - 9 * row;
 
 	// rows and columns
-	for (int i = 0; i < 9; i++)
-	{
+	for (int i = 0; i < 9; i++) {
 		if (board[i + row * 9] == board[index] && i != col)
 			return false;
 
@@ -31,10 +27,8 @@ bool Solver::isValid(int index)
 	int gridRow = row / 3;
 	int gridCol = col / 3;
 
-	for (int i = gridRow * 3; i < gridRow * 3 + 3; i++) 
-	{
-		for (int j = gridCol * 3; j < gridCol * 3 + 3; j++) 
-		{
+	for (int i = gridRow * 3; i < gridRow * 3 + 3; i++) {
+		for (int j = gridCol * 3; j < gridCol * 3 + 3; j++) {
 			if ((board[j + i * 9] == board[index]) && (i != row) && (j != col)) 
 				return false;
 		}
@@ -45,8 +39,7 @@ bool Solver::isValid(int index)
 
 bool Solver::isSolved()
 {
-	for (int i = 0; i < board.size(); i++)
-	{
+	for (int i = 0; i < board.size(); i++) {
 		if (isEmpty(i) == true || isValid(i) == false)
 			return false;
 	}
@@ -54,12 +47,9 @@ bool Solver::isSolved()
 	return true;
 }
 
-bool Solver::solve(int index)
-{
-	if (index == board.size())
-	{
-		if (isSolved() == true)
-		{
+bool Solver::solve(int index) {
+	if (index == board.size()) {
+		if (isSolved() == true) {
 			printBoard();
 			return true;
 		}
@@ -67,13 +57,10 @@ bool Solver::solve(int index)
 			return false; 
 	}
 
-	if (isEmpty(index) == true)
-	{
-		for (int i = 1; i <= 9; i++)
-		{
+	if (isEmpty(index) == true) {
+		for (int i = 1; i <= 9; i++) {
 			board[index] = i;
-			if (isValid(index) == true)
-			{
+			if (isValid(index) == true) {
 				bool node = solve(index + 1);
 				if (node == true) { return true; }
 				board[index] = 0;
@@ -88,11 +75,9 @@ bool Solver::solve(int index)
 	return node;
 }
 
-void Solver::printBoard()
-{
+void Solver::printBoard() {
 	std::cout << "\n";
-	for (int i = 0; i < board.size(); i++)
-	{
+	for (int i = 0; i < board.size(); i++) {
 		std::cout << board[i] << " ";
 		if ((i + 1) % 9 == 0 && i != 0)
 			std::cout << "\n";
